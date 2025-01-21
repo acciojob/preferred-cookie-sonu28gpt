84313d89fd7c503606599dea70e7ff82801a2540
sonu28gpt <98702109+sonu28gpt@users.noreply.github.com>
2025-01-21 22:14:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
 //your JS code here. If required.
+function getCookie(uname){
+	let arr=document.cookie.split('; ')
+		.map((el)=>{
+ 
+  
+         let key= el.split('=')[0]
+        let val=el.split('=')[1]
+        return {
+          [key]:val
+        }
+  
+  })
+		.filter((el)=>{
+		    if(el[uname]){
+		      return el
+			    }
+			  })
+	if(arr.length)return arr[0][uname];
+	return null;
+}
+
+document.addEventListener('DOMContentLoaded',()=>{
+	let fc=document.querySelector('#fontcolor')
+	let fs=document.querySelector('#fontsize')
+	let fsv=getCookie('fontsize')
+	let fcv=getCookie('fontcolor')
+	if(fsv){
+		fs.value=fsv;
+	}
+	if(fcv){
+		fc.value=fcv;
+	}
+})
+
+let submit=document.querySelector('input[type=submit]');
+submit.addEventListener('click',(e)=>{
+	e.preventDefault();
+	let form=document.querySelector('form');
+	let res=new FormData(form);
+	let {fontsize,fontcolor}=Object.fromEntries(res);
+	document.cookie=`fontsize=${fontsize};`
+	document.cookie=`fontcolor=${fontcolor};`
+})
